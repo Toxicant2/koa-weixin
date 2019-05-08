3322b73ba8875a294f8a415aa46e2c49fb8fbbbe
Toxicant2 <1192874863@qq.com>
2019-05-08 10:59:10 +0800

    update common weixin

--- common/weixin.js
+++ common/weixin.js
@@ -1,29 +1,40 @@
 import https from 'https'
 import config from '../config/index'
+
+function httpsRequest(URL) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(config.WxHost + URL, res => {
+            res.setEncoding('utf8')
+            res
+                .on('data', d => {
+                    resolve(JSON.parse(d))
+                })
+                .on('end', () => {
+                    // console.log('响应中已无数据。')
+                })
+        })
+        req.on('error', e => {
+            reject(e)
+            console.error('https error:', e.message)
+        })
+        req.end()
+    })
+}
 module.exports = {
     // 登录凭证校验。通过 wx.login() 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程
     code2Session(code) {
-        return new Promise((resolve, reject) => {
-            const URL = `https://api.weixin.qq.com/sns/jscode2session?appid=${
-                config.APPID
-            }&secret=${
-                config.SECRET
-            }&js_code=${code}&grant_type=authorization_code`
-            const req = https.get(URL, res => {
-                res.setEncoding('utf8')
-                res
-                    .on('data', d => {
-                        resolve(JSON.parse(d))
-                    })
-                    .on('end', () => {
-                        // console.log('响应中已无数据。')
-                    })
-            })
-            req.on('error', e => {
-                reject(e)
-                console.error('https error:', e.message)
-            })
-            req.end()
-        })
+        const URL = `sns/jscode2session?appid=${config.AppId}&secret=${
+            config.AppSecret
+        }&js_code=${code}&grant_type=authorization_code`
+
+        return httpsRequest(URL)
+    },
+
+    getAccessToken() {
+        const URL = `cgi-bin/token?grant_type=client_credential&appid=${
+            config.AppId
+        }&secret=${config.AppSecret}`
+
+        return httpsRequest(URL)
     }
 }
